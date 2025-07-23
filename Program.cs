a921764da7e20ce6fd5ab7997fda59c75d16ab93
rahmaAlmamari <alr816125@gmail.com>
2025-07-23 15:34:11 +0400

    Adding logic for rente a vehicle

--- Program.cs
+++ Program.cs
@@ -44,6 +44,69 @@ namespace PolymorphismTaskOOP
                 truck.PrintTruckDetails();
             }
 
+            //-------------------------------------------------------
+            //to ask the user to select a vehicle type ...
+            Console.WriteLine("\nSelect a vehicle type to rent:\n" + 
+                              "1. Car\n" + 
+                              "2. Motorbike\n" + 
+                              "3. Truck");
+            int vehicleType = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the number of days you want to rent the vehicle:");
+            int days = int.Parse(Console.ReadLine());
+            double rentalCost = 0;
+            switch (vehicleType)
+            {
+                case 1: // Car
+                    Console.WriteLine("Do you want a driver? (yes/no)");
+                    bool withDriverCar = Console.ReadLine().ToLower() == "yes";
+                    Console.WriteLine("Is the car luxury? (yes/no)");
+                    bool isLuxuryCar = Console.ReadLine().ToLower() == "yes";
+                    rentalCost = cars[0].CalculateRentalCost(days, withDriverCar, isLuxuryCar);
+                    if (rentalCost < 0)
+                    {
+                        Console.WriteLine("This car cannot be rented.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Total rental cost for the car: {rentalCost}");
+                    }
+                    break;
+                case 2: // Motorbike
+                    Console.WriteLine("Do you want a driver? (yes/no)");
+                    bool withDriverBike = Console.ReadLine().ToLower() == "yes";
+                    Console.WriteLine("Does the motorbike require a helmet? (yes/no)");
+                    bool requiresHelmet = Console.ReadLine().ToLower() == "yes";
+                    rentalCost = motorbikes[0].CalculateRentalCost(days, withDriverBike, requiresHelmet);
+                    if (rentalCost < 0)
+                    {
+                        Console.WriteLine("This motorbike cannot be rented.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Total rental cost for the motorbike: {rentalCost}");
+                    }
+                    break;
+                case 3: // Truck
+                    Console.WriteLine("Do you want a driver? (yes/no)");
+                    bool withDriverTruck = Console.ReadLine().ToLower() == "yes";
+                    Console.WriteLine("Enter the maximum load in kg:");
+                    double maxLoadKg = double.Parse(Console.ReadLine());
+                    rentalCost = trucks[0].CalculateRentalCost(days, withDriverTruck, maxLoadKg);
+                    if (rentalCost < 0)
+                    {
+                        Console.WriteLine("This truck cannot be rented.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Total rental cost for the truck: {rentalCost}");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid vehicle type selected.");
+                    break;
+            }
+
+
 
         }
     }
