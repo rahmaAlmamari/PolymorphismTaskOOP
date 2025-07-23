d99676aabf15bf86c2828d4a2d75143dc1493d32
rahmaAlmamari <alr816125@gmail.com>
2025-07-23 14:55:46 +0400

    Adding CalculateRentalCost method in car class

--- Car.cs
+++ Car.cs
@@ -12,6 +12,7 @@ namespace PolymorphismTaskOOP
         public bool IsLuxury;
         //2. class properties ...
         //3. class methods ...
+        //override method ...
         public override double CalculateRentalCost(int days)
         {
             // to check if Vehicle is older than 10 years ...
@@ -28,6 +29,33 @@ namespace PolymorphismTaskOOP
                 return days * 60; // Regular car rate
             }
         }
+        //overload method ...
+        public double CalculateRentalCost(int days, bool withDriver, bool is_luxury)
+        {
+            double cost = CalculateRentalCost(days); // Call the base method to get the cost
+            if (cost < 0) // If the vehicle is too old for rental
+            {
+                return -1; // Return -1 to indicate that the vehicle cannot be rented
+            }
+            if (withDriver && is_luxury)
+            {
+                cost += 100 * days * 80; // Add driver cost of 100 per day
+            }
+            else if (withDriver)
+            {
+                cost += 100 * days * 60; // Add driver cost of 100 per day for regular car
+            }
+            else if (is_luxury)
+            {
+                cost += 80 * days; // Additional luxury fee
+            }
+            else
+            {
+                cost += 60 * days; // Additional regular fee
+            }
+            return cost; // Return the total cost
+        }
+
         //4. class constructor ...
     }
 }
