4e817600951c9dcc09829c3b2f3c34dd009b5367
rahmaAlmamari <alr816125@gmail.com>
2025-07-23 15:03:24 +0400

    Adding CalculateRentalCost method in Motorbike method

--- Motorbike.cs
+++ Motorbike.cs
@@ -29,6 +29,32 @@ namespace PolymorphismTaskOOP
                 return days * 30; // Regular motorbike rate
             }
         }
+        //overload method ...
+        public double CalculateRentalCost(int days, bool withDriver, bool requiresHelmet)
+        {
+            double cost = CalculateRentalCost(days); // Call the base method to get the cost
+            if (cost < 0) // If the vehicle is too old for rental
+            {
+                return -1; // Return -1 to indicate that the vehicle cannot be rented
+            }
+            if (withDriver && requiresHelmet)
+            {
+                cost += 100 * days * 40; // Add driver cost of 100 per day for motorbike with helmet
+            }
+            else if (withDriver)
+            {
+                cost += 100 * days * 30; // Add driver cost of 100 per day for regular motorbike
+            }
+            else if (requiresHelmet)
+            {
+                cost += 40 * days; // Additional helmet fee
+            }
+            else
+            {
+                cost += 30 * days; // Additional regular fee
+            }
+            return cost; // Return the total cost
+        }
         //4. class constructor ...
     }
 }
